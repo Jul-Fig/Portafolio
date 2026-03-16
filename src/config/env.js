5e975208f851fba5c05dab1172d887da0914826c
Julian <figueredo.julian96@gmail.com>
2026-03-16 04:53:24 -0500

    Actualización cv, logo

--- src/config/env.js
+++ src/config/env.js
@@ -7,6 +7,6 @@ export const config = {
   },
   site: {
     title: "Julian Figueredo — Full Stack Developer",
-    cvPath: "/assets/data/CV_Julian_Lozada_Developer.pdf",
+    cvPath: "/assets/data/CV_Julian_Lozada_Desarrollador_Software.pdf",
   },
 };
