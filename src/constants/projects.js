b30a0a48f7a389faed5ca5c536aad1fe3194dc67
Julian <figueredo.julian96@gmail.com>
2026-03-25 13:21:31 -0500

    actualización img

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -5,19 +5,19 @@ export const PROJECTS = [
     id: 1,
     title: "TravelGo",
     description: "Sistema web full stack que conecta frontend y backend mediante APIs REST, implementando autenticación con JWT, manejo de base de datos relacional y lógica de negocio transaccional, construido con arquitectura en capas para soportar procesos reales, interacción eficiente entre servicios y experiencia de usuario.",
-    img: "/assets/projects/travelgoPortafolio.png",
+    img: "../../public/assets/projects/travelgoPortafolio.png",
     link: "https://github.com/Jul-Fig",
     github: "https://github.com/Jul-Fig/TravelGo",
-    tags: ["React", "Spring Boot", "MySQL", "JWT", "MVC"],
+    tags: ["React", "Spring Boot", "MySQL", "JWT"],
   },
   {
     id: 2,
     title: "Acortador de Links ",
     description: "Desarrollé un acortador de URLs listo para producción, con énfasis en seguridad, validación de datos y manejo eficiente de solicitudes. Incluye redirección optimizada, control de abusos y pruebas de integración, reflejando un enfoque práctico hacia sistemas confiables y escalables." ,
-    img: "/assets/projects/acortadorPortafolio.png",
+    img: "../../public/assets/projects/acortadorPortafolio.png",
     link: "https://acortador-de-links-jul-fig.netlify.app",
     github: "https://github.com/Jul-Fig/Acortador-de-links",
-    tags: ["Express", "Angular", "MongoDB", "JWT", "Jest" ],
+    tags: ["Express", "Angular", "MongoDB", "Jest"],
   },
 
 ];
